08c55c1d0146d86086523019a0c8ded36e443086
absorbyy <1695250547@qq.com>
2019-01-18 16:57:13 +0800

    init sell

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+<div class="app-container">
+<top></top>
+</div>
+</template>
+
+<script>
+import top from "./components/header/header.vue"
+export default {
+  data () {
+    return {
+
+    };
+  },
+  components:{
+    top
+  };
+}
+</script>
+
+<style lang="css" scoped>
+</style>
